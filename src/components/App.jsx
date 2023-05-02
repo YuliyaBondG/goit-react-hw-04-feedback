b7f738d6ff527e6d9212cc991791bc0b3a648945
YuliyaBondG <113913438+YuliyaBondG@users.noreply.github.com>
2023-05-02 23:00:40 +0600

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,63 @@
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeadbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  updateState = newFeedback => {
+    this.setState(currFeedback => {
+      return {
+        [newFeedback]: currFeedback[newFeedback] + 1,
+      };
+      // let obj = { ...currFeedback };
+      // obj[newFeedback] = currFeedback[newFeedback] + 1;
+      // return obj;
+    });
+  };
+  // загальна кількість відгуків
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  // % позитивних відгуків
+  countPositiveFeedbackPercentage = () => {
+    return Math.floor(
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+        100 || 0
+    );
+  };
+
+  render() {
+    return (
+      <div className={css.container}>
+        <Section title="Please Leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeavefeedback={this.updateState}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback yet..." />
+          ) : (
+            <Statistics
+              options={Object.keys(this.state)}
+              statistic={this.state}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
